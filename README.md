d63a16e009eebd3f8a26e86208f055e08f0d6e26
skipjac <skip@skipjack.info>
2013-10-29 10:04:59 -0700

    Uploading version 1

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-hide-forms-based-on-group
-=========================
+Hide forms options based on Agent group membership
+==================================================
 
-Hide and show ticket form options based on agent group membership
+Hide ticket form options based on the current agents group membership. The app will hide all the options that are set on the settings page as a comma separtated list. 
+
+``` javascript
+123,145,2000
+```
+
+In a JSON object create the group to allowed forms. 
+
+``` javascript 
+{"1": ["123", 145], "2": ["2000"]}
+```
---
+++ app.js
@@ -0,0 +1,37 @@
+(function() {
+
+  return {
+    events: {
+      'app.activated':'hideFormOption'
+    },
+    //hide fields on load
+    hideFormOption: function() {
+      //build a array of the fields to hide
+      var arrFormID = this.setting('formIDMap').split(',');
+      //hide the fields in the settings array when the app loads
+      arrFormID.forEach(function(key){
+        this.ticketFields('ticket_form_id').options(key).hide();
+      }, this);
+      this.showForm();
+    },
+    //build a array of group id's for the currently logged in user
+    userGroup: function(){
+      return _.map(this.currentUser().groups(), function(group){
+        return group.id().toString();
+      });
+    },
+    showForm: function(){
+      console.log(this.userGroup());
+      var groupMap = JSON.parse(this.setting('groupsIDMap') || '{}');
+      console.log(groupMap);
+      this.userGroup().forEach(function(key){
+        if(groupMap[key] !== undefined){
+          console.log(groupMap[key]);
+          groupMap[key].forEach(function(gKey){
+            this.ticketFields('ticket_form_id').options(gKey).show();
+          }, this);
+        }
+      }, this);
+    }
+  };
+}());
